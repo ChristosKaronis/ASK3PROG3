7e55fbe8278dc40b626a327c68d79c217040fc09
ChristosKaronis <christoskaranis@gmail.com>
2025-01-30 14:50:18 +0200

    ok

--- functions_2.c
+++ functions_2.c
@@ -5,8 +5,18 @@ void receive_shop_statistics(int sock) {
     char buffer[BUFFER_SIZE];
     //Clear buffer before receiving to avoid errors
     memset(buffer, 0, sizeof(buffer));
-    //receive data from server
-    recv(sock, buffer, sizeof(buffer) - 1, 0);
-    //Print them
-    printf("%s", buffer);
+
+// Continuously receive data from server until all statistics are received 
+//since it didnt print everything at start
+    int bytes_received;
+    while ((bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytes_received] = '\0'; 
+        printf("%s", buffer); 
+        //Clear buffer for next data
+        memset(buffer, 0, sizeof(buffer)); 
+    }
+
+    if (bytes_received < 0) {
+        perror("Error receiving shop statistics");
+    }
 }
